3b8ea5e7af94bbde407ef7cdae58ce4be88b01e9
Bhaskar Thadisetty <16bhaskar@gmail.com>
2016-02-11 12:50:07 -0500

    WordCount

---
+++ wordcount.file.py
@@ -0,0 +1,27 @@
+from mapreduce import MapReduce
+
+#----------------------------------------
+
+class WordCount(MapReduce):
+
+    def mapper(self, _, line):
+        for word in line.split():
+            yield (word,1)
+
+    def combiner(self, key, values):
+            yield key, sum(values)
+
+    def reducer(self, key, values):
+        yield key, sum(values)
+
+#----------------------------------------
+
+
+#
+
+input = open("twitter_data_12_16.txt").readlines()
+
+output = WordCount.run(input)
+for item in output:
+    print(item)
+
